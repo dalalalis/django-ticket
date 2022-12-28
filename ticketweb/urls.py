cb38f32a64c12bc3169d9c7e211a2a89fa2bc7dd
Haitham <haaa555@outlook.com>
2022-12-28 15:52:25 +0300

    almost finish

--- ticketweb/urls.py
+++ ticketweb/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import index,ticket_details
+from .views import index,ticket_details,add_to_cart,my_cart, remove_ticket_from_cart,profile,buy_the_cart,add_new_ticket
 
 urlpatterns = [
     path('', index, name='index'),
     path('ticket/detail/<int:ticket_id>', ticket_details, name='ticket_details_view'),
+    path('ticket/addtocart/<int:ticket_id>', add_to_cart, name='ticket_add_to_cart'),
+    path('cart', my_cart, name='my_cart'),
+    path('cart/checkout', buy_the_cart, name='buy_cart'),
+    path('cart/remove/<int:ticket_id>', remove_ticket_from_cart, name='remove_ticket_from_cart'),
+    path('profile/', profile, name='my_profile'),
+    path('profile/add_ticket/', add_new_ticket, name='add_new_ticket'),
 ]
 
-    
